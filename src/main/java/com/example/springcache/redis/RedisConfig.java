497794aa1d38bc50a9cd28cd5e7d54f761f9dfb8
Choi Tae gyu <suker800@gmail.com>
2022-12-27 21:25:24 +0900

    RedisConfig 호스트 포트 설정

--- src/main/java/com/example/springcache/redis/RedisConfig.java
+++ src/main/java/com/example/springcache/redis/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.example.springcache.redis;
 
 import com.example.springcache.user.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,10 +12,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    private final String host;
+    private final int port;
+
+    public RedisConfig(final @Value("${spring.redis.host}") String host , final @Value("{spring.redis.port}") int port) {
+        this.host = host;
+        this.port = port;
+    }
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();  // Lettuce 사용
+        return new LettuceConnectionFactory(host, port);  // Lettuce 사용
     }
 
     @Bean
